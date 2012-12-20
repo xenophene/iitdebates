6b72b44bd767128ec3c6ef606ced8be60e07c4b3
Ravee Malla <ravmalla@gmail.com>
2012-12-20 22:17:44 +0530

    removed extra-files

--- includes/remove-comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* remove the comment requested by the user */
-include 'config.php';
-
-$comid = $_POST['comid'];
-$query = "DELETE FROM `comments` WHERE `comid`='$comid'";
-$result = $conn->query($query);
-
-?>
